09a917793644176ddc02017583dcac8978ecfb0a
kliuchyk <kliuchyk@gmail.com>
2021-03-11 09:57:50 +0200

    add client app basic structure

---
+++ client/src/components/AppFooter/index.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const AppFooter = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AppFooter;
---
+++ client/src/pages/CountryDetailsPage/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountryDetailsPage = () => {
+  return <div></div>;
+};
+
+export default CountryDetailsPage;
